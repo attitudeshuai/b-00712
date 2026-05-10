abc190f1ae8026f0c8f13b706d1fdc4db62ef796
李丽仙 <lilixian@benzhi.io>
2026-05-10 12:02:51 +0800

    TestM_2

--- src/js/member.js
+++ src/js/member.js
@@ -17,7 +17,7 @@ layui.use(['table', 'form', 'layer'], function(){
         data: members,
         cols: [[
             {field: 'id', title: '编号', width: 80, sort: true},
-            {field: 'name', title: '姓名', width: 100},
+            {field: 'name', title: '姓名', width: 100, event: 'viewDetail', style: 'color: #1E9FFF; cursor: pointer;'},
             {field: 'studentId', title: '学号', width: 120, sort: true},
             {field: 'gender', title: '性别', width: 60},
             {field: 'grade', title: '年级', width: 100},
@@ -51,7 +51,48 @@ layui.use(['table', 'form', 'layer'], function(){
     // Tool Bar Events
     table.on('tool(memberTable)', function(obj){
         const data = obj.data;
-        if(obj.event === 'del'){
+        if(obj.event === 'viewDetail'){
+            const allClubs = JSON.parse(localStorage.getItem(App.STORAGE_KEYS.clubs) || '[]');
+            const club = allClubs.find(c => c.id === data.clubId) || {};
+
+            let html = '<div style="padding:15px;">'
+                + '<table class="layui-table" style="margin:0;">'
+                + '<colgroup><col width="100"><col></colgroup>'
+                + '<tbody>'
+                + '<tr><td style="font-weight:bold;">姓名</td><td>' + data.name + '</td></tr>'
+                + '<tr><td style="font-weight:bold;">学号</td><td>' + data.studentId + '</td></tr>'
+                + '<tr><td style="font-weight:bold;">性别</td><td>' + data.gender + '</td></tr>'
+                + '<tr><td style="font-weight:bold;">年级</td><td>' + data.grade + '</td></tr>'
+                + '<tr><td style="font-weight:bold;">所属社团</td><td>' + (data.clubName || '') + '</td></tr>'
+                + '<tr><td style="font-weight:bold;">职位</td><td>' + data.position + '</td></tr>'
+                + '<tr><td style="font-weight:bold;">入社时间</td><td>' + data.joinTime + '</td></tr>'
+                + '<tr><td style="font-weight:bold;">联系电话</td><td>' + (data.phone || '') + '</td></tr>'
+                + '</tbody></table>';
+
+            if(club && club.id){
+                html += '<fieldset class="layui-elem-field" style="margin-top:20px;">'
+                    + '<legend>所在社团信息</legend>'
+                    + '<div class="layui-field-box" style="padding:10px;">'
+                    + '<table class="layui-table" style="margin:0;">'
+                    + '<colgroup><col width="100"><col></colgroup>'
+                    + '<tbody>'
+                    + '<tr><td style="font-weight:bold;">社团名称</td><td>' + club.name + '</td></tr>'
+                    + '<tr><td style="font-weight:bold;">负责人</td><td>' + club.leader + '</td></tr>'
+                    + '<tr><td style="font-weight:bold;">联系方式</td><td>' + club.phone + '</td></tr>'
+                    + '</tbody></table>'
+                    + '</div></fieldset>';
+            }
+
+            html += '</div>';
+
+            layer.open({
+                type: 1,
+                title: data.name + ' - 成员详情',
+                area: ['480px', '500px'],
+                shadeClose: true,
+                content: html
+            });
+        } else if(obj.event === 'del'){
             layer.confirm('真的删除行么', function(index){
                 // Delete from localStorage
                 members = members.filter(m => m.id !== data.id);
